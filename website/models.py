a62bc2860aed8c46cebc36147e76f2a5a8c15a33
Sam Pearson <sam.ed.pearson@gmail.com>
2022-11-22 15:59:59 -0600

    login/signup functionality

---
+++ website/models.py
@@ -0,0 +1,22 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    # The foreign key below refers to User class despite being in lower case
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    # Announcing a relationship with the Note class and storing linked Notes
+    # Note that the class Note is capitalized here, Note's reference to this class is lower case.
+    notes = db.relationship('Note')
+
