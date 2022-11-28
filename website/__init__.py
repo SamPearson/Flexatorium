6160a64b9a16d119a60a6f94c9ce3a06575ce030
Sam Pearson <sam.ed.pearson@gmail.com>
2022-11-28 11:47:31 -0600

    Adding/viewing/deleting notes, only showing notes when logged in

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 from dotenv import load_dotenv
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -24,5 +25,13 @@ def create_app():
 
     with app.app_context():
         db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return models.User.query.get(int(user_id))
 
     return app
