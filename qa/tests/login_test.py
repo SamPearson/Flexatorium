435281f560eb844ea2f0dcf890f63af35c358772
Sam Pearson <sam.ed.pearson@gmail.com>
2022-12-03 16:13:01 -0600

    First test and test configuration

---
+++ qa/tests/login_test.py
@@ -0,0 +1,19 @@
+import pytest
+from pages import login_page
+
+
+@pytest.mark.login
+class TestLogin:
+    @pytest.fixture
+    def login(self, driver):
+        return login_page.LoginPage(driver)
+
+    @pytest.mark.smoke
+    def test_valid_credentials(self, login):
+        login.send_credentials("bill@partycentral.net", "sillybilly")
+        assert login.success_message_present()
+
+    @pytest.mark.midweight
+    def test_invalid_credentials(self, login):
+        login.send_credentials("bademail@addr.ess", "badpassword")
+        assert login.failure_message_present()
