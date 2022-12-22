dfd5630923c10a5b272cf9211e35c0564dc0aabc
Sam Pearson <sam.ed.pearson@gmail.com>
2022-12-22 13:25:26 -0600

    Adding QA hook classes

--- website/static/exercises.js
+++ website/static/exercises.js
@@ -9,14 +9,14 @@ $("body").on("click", ".delete-config-option-button", function() {
 $(".add-config-option-button").click(function(){
     var count = parseInt( $('.config-options').attr("option-count") );
     newRow =
-        '<div class="form-group row config-option-row">' +
+        '<div class="form-group row config-option-row config-option-field">' +
         '<div class="col">' +
           '<input name="config_types-' + count +
-          '" type="text" class="form-control" placeholder="Config Type (eg weight)">' +
+          '" type="text" class="form-control config-type-field" placeholder="Config Type (eg weight)">' +
         '</div>' +
         '<div class="col">' +
           '<input name="config_unit_names-' + count +
-          '" type="text" class="form-control" placeholder="Unit Name (eg lbs)">' +
+          '" type="text" class="form-control config-unit-field" placeholder="Unit Name (eg lbs)">' +
         '</div>' +
         '<div class="col">' +
           '<button type="button" class="btn btn-danger delete-config-option-button">Remove</button>' +
