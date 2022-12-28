7793464230f9c5407eb1d377c65688b259d642a4
Sam Pearson <sam.ed.pearson@gmail.com>
2022-12-28 08:57:57 -0600

    signup page object and a qa hook class for the signup page's submit button

--- qa/pages/signup_page.py
+++ qa/pages/signup_page.py
@@ -1,28 +1,37 @@
 from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
+import random,string
 
 
-class LoginPage(BasePage):
-    _login_form = {"by": By.ID, "value": "login-form"}
+class SignupPage(BasePage):
+    _signup_form = {"by": By.ID, "value": "signup-form"}
+    _username_input = {"by": By.CLASS_NAME, "value": "username-field"}
     _email_input = {"by": By.CLASS_NAME, "value": "email-field"}
     _password_input = {"by": By.CLASS_NAME, "value": "password-field"}
+    _password_confirmation = {"by": By.CLASS_NAME, "value": "password-confirm-field"}
     _submit_button = {"by": By.CLASS_NAME, "value": "submit-button"}
 
-    _success_message = {"by": By.ID, "value": "success-notif"}
-    _failure_message = {"by": By.ID, "value": "failure-notif"}
+    _username_error = {"by": By.CLASS_NAME, "value": "username-error"}
+    _email_error = {"by": By.CLASS_NAME, "value": "email-error"}
+    _password_error = {"by": By.CLASS_NAME, "value": "password-error"}
 
     def __init__(self, driver):
         self.driver = driver
-        self._visit("/login")
-        assert self._is_displayed(self._login_form)
+        self._visit("/sign-up")
+        assert self._is_displayed(self._signup_form)
 
-    def send_credentials(self, email, password):
+    def fill_registration_form(self, username, email, password, password2):
+        self._type(self._username_input, username)
         self._type(self._email_input, email)
         self._type(self._password_input, password)
+        self._type(self._password_confirmation, password2)
+
+    def register(self, username, email, password):
+        self.fill_registration_form(username, email, password, password)
         self._click(self._submit_button)
 
-    def success_message_present(self):
-        return self._is_displayed(self._success_message, 1)
+    def password_error_present(self):
+        return bool(self._find(self._password_error))
 
-    def failure_message_present(self):
-        return self._is_displayed(self._failure_message, 1)
+    def password_error(self):
+        return self._find(self._password_error).get_text()
